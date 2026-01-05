bc494ffa521b3e85c583ec9be05c32dd09a770fb
pino <uni.nova.tih@gmail.com>
2026-01-05 14:12:07 +0100

    init

---
+++ swiftui-samples/swiftui_samplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_samplesApp.swift
+//  swiftui-samples
+//
+//  Created by pino on 05.01.26.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_samplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
